dcd6da210e49de34da3aed114cf2799ff7377860
Chakib Moussa <shakbec@gmail.com>
2018-02-13 17:42:31 +0300

    Last verstion

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -1,10 +1,6 @@
 # Write a program that simulates an ATM. The user will request an amount
 #and the program will give the user money notes based on the available
 #notes that can be printed (100, 50, 10, 5, rest of money)
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 # write a definition that takes the amount as input and process the required output
 
 def to_notes(n):
@@ -27,7 +23,6 @@ def to_notes(n):
     if n > 0:
         print "Give " + str(n)
 
-<<<<<<< HEAD
 money = 500
 request = 488
 
@@ -37,12 +32,3 @@ if request > 500:
     print "Request is over than 500, kindly request 500 or less"
 else:
     to_notes(request)
-=======
-
-to_notes(277)
-
-
-
-
-
->>>>>>> master
