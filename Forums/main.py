39aa3f1e74b60b2e7ba8b082348f05b088b5898d
Chakib Moussa <shakbec@gmail.com>
2018-02-16 06:30:27 +0300

    First Assignment of Forums
    Created two files models.py and main.py as instructed by Mr.Ahmed in the OMAC lab

---
+++ Forums/main.py
@@ -0,0 +1,8 @@
+from models import *
+
+member1 = Member("ahmed",24)
+member2 = Member("khalid",24)
+
+post1 = Post("First Post","This is the first post ever.")
+post2 = Post("Python Programming", "Python programming is fun.")
+post3 = Post("Models and Instances", "This part of coding is very interesting")
---
+++ Forums/models.py
@@ -0,0 +1,15 @@
+class Member():
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+
+class Post():
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
