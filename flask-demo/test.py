435101c9f70c9a979f6bcaa991657e8b721009ea
Chakib Moussa <shakbec@gmail.com>
2018-03-06 11:21:00 +0300

    Add flask-demo project

---
+++ flask-demo/test.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route("/index/")
+def home():
+    return "Hi there"
+
+
+@app.route("/SayHello/<user>/")
+def hello(user):
+    return 'Hello %s' % user
+
+
+app.run()
