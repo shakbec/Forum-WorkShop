7c79d66089f893abfca7e4e2e9e74f8ed29af767
Chakib Moussa <shakbec@gmail.com>
2018-03-07 03:26:09 +0300

    Add course 30

--- Forums/app.py
+++ Forums/app.py
@@ -1,12 +1,12 @@
 from flask import render_template, Flask
-
+import dummy_data
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=dummy_data.post_store.get_all())
 
 
 app.run()
